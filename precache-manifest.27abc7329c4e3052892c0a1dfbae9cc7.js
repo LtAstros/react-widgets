211dba000ea4058e06aeccc992f2ff030215329b
Costin Smilovici <costinsmilovici@Costin-MacBook-Air-128GB.local>
2020-09-24 16:27:38 -0700

    Updates

---
+++ precache-manifest.27abc7329c4e3052892c0a1dfbae9cc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab7c04313d01f5ac2bce01eb3b738b2",
+    "url": "/react-widgets/index.html"
+  },
+  {
+    "revision": "322e535288e1e8a3943b",
+    "url": "/react-widgets/static/js/2.4fdabdf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-widgets/static/js/2.4fdabdf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e525de03a91400005f3f",
+    "url": "/react-widgets/static/js/main.8799349b.chunk.js"
+  },
+  {
+    "revision": "af9080dd027d4969290e",
+    "url": "/react-widgets/static/js/runtime-main.271165c5.js"
+  }
+]);
